40d008fc3856cb72a0d378ab18ed4199ff58f9dd
Ridma Dilshan <103328122+avRidmaDilshan@users.noreply.github.com>
2023-09-13 08:44:57 +0530

    Fix the Updates

--- src/config/database.providers.ts
+++ src/config/database.providers.ts
@@ -13,17 +13,10 @@ import { lc_tr } from "../entity/lcttModel"
 const myDataSource = new DataSource({
   type: "mysql",
   host: "localhost",
-<<<<<<< HEAD
   username: "root",
   password: "1234",
   database: "wheels1",
   synchronize: true,
-=======
-  username: "admin",
-  password: "password",
-  database: "WHEELS1",
-  synchronize: false,
->>>>>>> 24f0515a45c71e3d978702cf962f9cb3d678f48c
   logging: true,
   entities: [Post, Category,Order,OrderItems, Users,Performa, PerformaItems,Shipment, ShipmentItems,Clearnce,lc_tr],
   subscribers: [],
